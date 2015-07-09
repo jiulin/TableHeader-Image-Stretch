f220f8dee448420fb0b112d14019a5144b21a3f9
Git <sj901127@163.com>
2015-07-10 00:28:21 +0800

    Initial Commit

---
+++ ImageStretch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageStretch
+//
+//  Created by Vicky on 7/10/15.
+//  Copyright (c) 2015 Jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageStretch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageStretch
+//
+//  Created by Vicky on 7/10/15.
+//  Copyright (c) 2015 Jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
